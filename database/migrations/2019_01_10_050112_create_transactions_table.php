36525d9123b2f3c28617524213dca2e1e418e71c
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-01-12 22:02:34 +0700

    add foreign key user_id on table transactions

--- database/migrations/2019_01_10_050112_create_transactions_table.php
+++ database/migrations/2019_01_10_050112_create_transactions_table.php
@@ -23,6 +23,9 @@ class CreateTransactionsTable extends Migration
 
             $table->integer('wallet_id')->unsigned();
             $table->foreign('wallet_id')->references('id')->on('wallets')->onDelete('cascade');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
